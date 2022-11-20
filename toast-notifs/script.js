b5c876f5b81bd6432a15ec952d9de63aa1e6b640
akash12patel12 <akash12patel12@gmail.com>
2022-11-20 16:36:34 +0530

    toast notification

---
+++ toast-notifs/script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+
+btn.addEventListener('click', ()=>{
+    createNotif();
+})
+
+function createNotif(){
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.innerText = "This is toast"
+    container.appendChild(notif);
+    setTimeout(()=>{
+       notif.remove();
+    }, 2000);
+}
